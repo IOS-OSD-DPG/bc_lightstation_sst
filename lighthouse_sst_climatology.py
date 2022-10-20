6019b5999c1f5a2cecf3d8a3579256e36bd740dd
Hana Hourston <hana.hourston@gmail.com>
2022-10-20 16:57:51 -0700

    range check and climatology for sst

---
+++ lighthouse_sst_climatology.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import numpy as np
+import glob
+from os.path import basename
+
+# Use the monthly mean data already provided to compute the 30-year means
+
+parent_dir = 'C:\\Users\\HourstonH\\Documents\\charles\\our_warming_ocean\\' \
+             'lighthouse_data\\'
+
+file_list_dupe = glob.glob(
+    parent_dir +
+    'DATA_-_Active_Sites\\*\\*Average_Monthly_Sea_Surface_Temperatures*.csv')
+
+# Remove all french repeat files
+file_list = [f for f in file_list_dupe if 'french' not in f]
+file_list.sort()
+
+# climatology for 1991-2020
+clim_df = pd.DataFrame(
+    index=[basename(f) for f in file_list],
+    columns=pd.read_csv(file_list[0], skiprows=1, index_col='YEAR').columns)
+
+for f in file_list:
+    dfin = pd.read_csv(f, skiprows=1, index_col='YEAR', na_values=999.99)
+    clim_df.loc[basename(f), :] = dfin.loc[1991:2020].mean(axis=0)
+
+output_dir = parent_dir + 'climatological_monthly_means\\'
+clim_file_name = output_dir + 'lighthouse_sst_climatology_1991-2020.csv'
+clim_df.to_csv(clim_file_name)
---
+++ lighthouse_sst_range_check.py
@@ -0,0 +1,42 @@
+import numpy as np
+import pandas as pd
+import glob
+from os.path import basename
+
+COAST_N_PAC_RANGE_0m = (-2.10, 35.00)  # degrees C
+
+parent_dir = 'C:\\Users\\HourstonH\\Documents\\charles\\our_warming_ocean\\lighthouse_data\\'
+
+input_files_dupe = glob.glob(parent_dir + 'DATA_-_Active_Sites\\*\\*Daily*.csv')
+
+input_files = [f for f in input_files_dupe if 'french' not in f]
+input_files.sort()
+
+output_dir = parent_dir + 'range_check\\'
+
+# Name summary statistics file to output
+summary_stats_file = output_dir + 'range_check_summary_stats.csv'
+summary_stats_df = pd.DataFrame(
+    index=[basename(f) for f in input_files],
+    columns=['Num_input_obs', 'Num_nan_obs', 'Num_flagged_obs',
+             'fraction_passing_obs'])
+
+# Iterate through the files doing a range check on each observation
+for f in input_files:
+    dfin = pd.read_csv(f, skiprows=1, na_values=999.9)
+    dfin['T_range_flag'] = np.logical_or(
+        dfin.loc[:, 'TEMPERATURE ( C )'] < COAST_N_PAC_RANGE_0m[0],
+        dfin.loc[:, 'TEMPERATURE ( C )'] > COAST_N_PAC_RANGE_0m[1])  # np.zeros(len(dfin))
+
+    dfout = dfin.loc[~dfin.loc[:, 'T_range_flag'],
+                     ['DATE (YYYY-MM-DD)', 'TEMPERATURE ( C )']]
+
+    summary_stats_df.loc[basename(f), :] = [
+        len(dfin), sum(dfin.loc[:, 'TEMPERATURE ( C )'].isna()),
+        sum(dfin.loc[:, 'T_range_flag']),
+        sum(~dfin.loc[:, 'T_range_flag']) / len(dfin)]
+
+    output_name = output_dir + basename(f)
+    dfout.to_csv(output_name, index=False)
+
+summary_stats_df.to_csv(summary_stats_file)
