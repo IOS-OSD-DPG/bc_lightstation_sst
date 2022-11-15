bb0fc827b55b427f7abaaae052d2786a802aa670
Hana Hourston <hana.hourston@gmail.com>
2022-11-14 17:17:52 -0800

    plot daily sst data with running mean and trend

--- plot_lighthouse_temperature.py
+++ plot_lighthouse_temperature.py
@@ -3,26 +3,55 @@ import pandas as pd
 import os
 import glob
 import numpy as np
+from patsy import dmatrices
+import statsmodels.api as sm
 
 
-def plot_lighthouse_t(anom_file, station_name, subplot_letter, plot_name,
-                      best_fit: bool):
-    anom_df = pd.read_csv(anom_file, index_col=[0])
-    n_years = len(anom_df)
+def lstq_model(anom_1d, date_numeric_1d):
+    # https://www.statsmodels.org/stable/gettingstarted.html
+
+    # Do not include nan values in the dataframe for the model
+    dfmod = pd.DataFrame(
+        {'Date': date_numeric_1d[~pd.isna(anom_1d)],
+         'Anomaly': anom_1d[~pd.isna(anom_1d)]}
+    )
+
+    # create design matrices
+    y, X = dmatrices('Anomaly ~ Date', data=dfmod, return_type='dataframe')
+
+    mod = sm.OLS(y, X)  # Describe model
+
+    res = mod.fit()  # Fit model, return regression results
+
+    # print(res.summary())  # Summarize model
+    return res
+
+
+def flatten_date(all_years):
+    # Convert the names of the months to numeric
+
+    n_years = len(all_years)
     n_months = 12
 
-    # station_name = anom_file.split('_')[0] + ' ' + anom_file.split('_')[1]
-
-    # Flatten the data into 1 dim for plot
-    # Convert the names of the months to numeric
     month_numeric = np.linspace(0, 1, 12 + 1)[:-1]
 
     date_numeric = np.zeros(shape=(n_years, len(month_numeric)))
     for i in range(n_years):
         for j in range(n_months):
-            date_numeric[i, j] = anom_df.index[i] + month_numeric[j]
+            date_numeric[i, j] = all_years[i] + month_numeric[j]
 
     date_flat = date_numeric.flatten()
+    return date_flat
+
+
+def plot_lighthouse_t(anom_file, station_name, subplot_letter, plot_name,
+                      best_fit: bool):
+    anom_df = pd.read_csv(anom_file, index_col=[0])
+
+    # station_name = anom_file.split('_')[0] + ' ' + anom_file.split('_')[1]
+
+    # Flatten the data into 1 dim for plot
+    date_flat = flatten_date(anom_df.index)
     anom_flat = anom_df.to_numpy().flatten()
 
     fig, ax = plt.subplots(figsize=[6, 3])  # width, height [6,3] [3.12, 1.56]
@@ -39,6 +68,22 @@ def plot_lighthouse_t(anom_file, station_name, subplot_letter, plot_name,
             date_nonan, anom_nonan, deg=1)
         x_linspace, y_hat_linspace = poly.linspace(n=100)
         ax.plot(x_linspace, y_hat_linspace, c='k')
+        # # Plot confidence limits around the linear fit
+        # # ci is some confidence interval
+        # ci = None
+        # ax.fill_between(x_linspace, (y_hat_linspace - ci),
+        #                 (y_hat_linspace - ci), c='b', alpha=0.1)
+
+        # STATSMODELS
+        # regres = lstq_model(anom_flat, date_flat)
+        # # Check if the linear trend is significant before plotting
+        # if all(regres.pvalues < 0.05):
+        #     ax.plot(date_nonan, regres.fittedvalues, c='r')
+        #     ax.fill_between(
+        #         date_nonan,
+        #         regres.fittedvalues - regres.conf_int().loc['Date', 0],
+        #         regres.fittedvalues + regres.conf_int().loc['Date', 1],
+        #         c='r', alpha=0.1)
 
     # Plot formatting
     ax.set_xlim((min(date_flat), max(date_flat)))
@@ -84,6 +129,161 @@ def plot_climatology(clim_file, output_dir):
     return
 
 
+def plot_filled_sst_resid(anom_file, all_time_mean, plot_name):
+    # Replicate a plot that Peter C shared in his 2022 SOPO presentation
+    # for all 8 lighthouse stations
+    anom_df = pd.read_csv(anom_file, index_col=[0])
+
+    station_name = os.path.basename(anom_file).split('_')[0] + ' ' + os.path.basename(anom_file).split('_')[1]
+    print(station_name)
+
+    # Flatten the data into 1 dim for plot
+    date_flat = flatten_date(anom_df.index)
+    anom_flat = anom_df.to_numpy().flatten()
+
+    # Make the least squares linear model
+    res = lstq_model(anom_flat, date_flat)
+
+    # Make the plot
+    fig, ax = plt.subplots(figsize=[6, 3])
+    # Move trend line down
+    ax.plot(date_flat[~pd.isna(anom_flat)], res.fittedvalues - 3, c='k', linewidth=3)
+    ax.fill_between(date_flat[~pd.isna(anom_flat)],
+                    res.resid,
+                    np.zeros(len(anom_flat[~pd.isna(anom_flat)])),
+                    where=res.resid > 0,
+                    color='r')
+    ax.fill_between(date_flat[~pd.isna(anom_flat)],
+                    res.resid,
+                    np.zeros(len(anom_flat[~pd.isna(anom_flat)])),
+                    where=res.resid < 0,
+                    color='b')
+    # Adjust plot bounds
+    ybot, ytop = plt.ylim()
+    ax.set_ylim((-max(abs(np.array([ybot, ytop]))) - 1,
+                 max(abs(np.array([ybot, ytop]))) + 1))
+    ax.set_xlim((np.nanmin(date_flat), np.nanmax(date_flat)))
+    # Adjust tick direction
+    plt.tick_params(which='major', direction='in',
+                    bottom=True, top=True, left=True, right=True)
+    # Add grid
+    ax.grid(color='grey', alpha=0.5)
+    # Label axes
+    ax.set_ylabel('Temperature Anomaly')
+    # Add text to plot
+    plt.text(
+        0.03, 0.89,
+        station_name + ' mean temperature ' + str(np.round(all_time_mean, 2)) +
+        '$^{\circ}$C, trend ' + str(np.round(res.params.Date * 100, 2)) + '$^{\circ}$C/100y',
+        transform=ax.transAxes, backgroundcolor='white')
+    # Save fig
+    plt.tight_layout()
+    plt.savefig(plot_name)
+    plt.close(fig)
+    return
+
+
+def plot_filled_daily_sst(daily_file, plot_name):
+    # Translated from Peter Chandler's matlab script
+
+    station_name = os.path.basename(daily_file).split('_')[0] + ' ' + os.path.basename(daily_file).split('_')[1]
+    print(station_name)
+
+    # Only read in date and temperature columns
+    df = pd.read_csv(daily_file, skiprows=1, na_values=[999.9, 999.99], usecols=[0, 2])
+    # Remove any rows that are all nans (Langara problem)
+    df.dropna(axis='index', how='all', inplace=True)
+    # Compute mean temperature for all time
+    mean_temp = df.loc[:, 'TEMPERATURE ( C )'].mean(skipna=True)
+
+    # Check for missing data
+    percent_missing_data = sum(df.loc[:, 'TEMPERATURE ( C )'].isna()) / len(df)
+    print('{:.2f}'.format(percent_missing_data), 'percent missing data')
+
+    # Replace nan in temperature time series with "normal" value
+    df.loc[df.loc[:, 'TEMPERATURE ( C )'].isna(), 'TEMPERATURE ( C )'] = mean_temp
+
+    # Pad the time series before and after with normal values to smooth edges
+    df['DATE (YYYY-MM-DD)'] = pd.to_datetime(df['DATE (YYYY-MM-DD)'])
+    df['DATE (float year)'] = df.loc[:, 'DATE (YYYY-MM-DD)'].dt.year + \
+                              df.loc[:, 'DATE (YYYY-MM-DD)'].dt.dayofyear / 365
+    # x = np.concatenate((
+    #     -pd.timedelta_range(start='1 day', periods=365).sort_values(ascending=False)
+    #     + df.loc[0, 'DATE (YYYY-MM-DD)'],
+    #     df.loc[:, 'DATE (YYYY-MM-DD)'].to_numpy(),
+    #     pd.timedelta_range(start='1 day', periods=365)
+    #     + df.loc[len(df) - 1, 'DATE (YYYY-MM-DD)']
+    # )).
+    x = np.concatenate((
+        -np.arange(365, 0, -1)/365 + df.loc[0, 'DATE (float year)'],
+        df.loc[:, 'DATE (float year)'].to_numpy(),
+        np.arange(1, 366, 1)/365 + df.loc[len(df) - 1, 'DATE (float year)']))
+    y = np.concatenate((
+        np.repeat(mean_temp, 365),
+        df.loc[:, 'TEMPERATURE ( C )'].to_numpy(),
+        np.repeat(mean_temp, 365)
+    ))
+
+    # Lowpass filter on daily temp data over 365 days
+    yy = pd.Series(
+        data=y
+    ).rolling(window=365, win_type='boxcar').mean().to_numpy()
+
+    # Remove beginning and end pads
+    x = x[365:len(x) - 366]
+    yy = yy[365:len(yy) - 366]
+
+    # Calculate the long-term trend
+    poly = np.polynomial.Polynomial.fit(x, yy, deg=1)
+    # Reduce data points to yearly representers
+    x2, y2 = poly.linspace(n=len(x[::365]))
+    # Calculate trend in deg C per 100 y
+    # trend = (y2[-1] - y2[0])/(x2[-1] - x2[0]) * 100  # by hand
+    trend = poly.convert().coef[1] * 100
+
+    # Make the plot
+
+    fig, ax = plt.subplots(figsize=[7, 3])
+    ax.fill_between(x, yy - mean_temp, np.zeros(len(yy)),
+                    where=yy - mean_temp > 0, color='r')
+    ax.fill_between(x, yy - mean_temp, np.zeros(len(yy)),
+                    where=yy - mean_temp < 0, color='b')
+    ax.plot(x2, y2 - 1 - mean_temp, c='k', linewidth=3)
+
+    # # Check poly.coef value
+    # ax.plot(x2, poly.coef[0] + poly.coef[1]*x2 - 1 - mean_temp, c='green')
+
+    ax.set_xlim((np.nanmin(x), np.nanmax(x)))
+    # Adjust tick direction
+    plt.tick_params(which='major', direction='in',
+                    bottom=True, top=True, left=True, right=True)
+    # Add grid
+    ax.grid(color='grey', alpha=0.5)
+    # Label axes
+    ax.set_ylabel('Temperature Anomaly')
+    rounded_mean = np.round(mean_temp, 2) if mean_temp < 10 else np.round(mean_temp, 1)
+    plt.text(
+        0.03, 0.89,
+        station_name + ' mean temperature ' + str(rounded_mean) +
+        '$^{\circ}$C, trend ' + '{:.2f}'.format(trend) + '$^{\circ}$C/100y',
+        transform=ax.transAxes, backgroundcolor='white')
+
+    # Save fig
+    plt.tight_layout()
+    plt.savefig(plot_name)
+    plt.close(fig)
+    return
+
+
+def sst_all_time_mean(monthly_mean_file):
+    df = pd.read_csv(monthly_mean_file, skiprows=1, index_col=[0],
+                     na_values=[999.9, 999.99])
+    # Take mean of mean
+    all_time_mean = df.mean(axis='index').mean()
+
+    return np.round(all_time_mean, 2) if all_time_mean < 10 else np.round(all_time_mean, 1)
+
+
 parent_dir = 'C:\\Users\\HourstonH\\Documents\\charles\\' \
              'our_warming_ocean\\lighthouse_data\\'
 
@@ -103,12 +303,13 @@ anom_files.sort()
 station_names.sort()
 
 for k in range(len(anom_files)):
-    png_filename = data_dir + station_names[k].replace(' ', '_') + '_monthly_mean_sst_anomalies.png'
+    png_filename = data_dir + station_names[k].replace(' ', '_') + '_monthly_mean_sst_anomalies_ols.png'
 
     plot_lighthouse_t(anom_files[k], station_names[k],
                       subplot_letter=subplot_letters[k],
                       plot_name=png_filename, best_fit=True)
 
+"""
 # Differences
 diff_dir = parent_dir + 'monthly_anom_differences\\'
 diff_files = glob.glob(diff_dir + '*.csv')
@@ -120,7 +321,6 @@ for k in range(len(diff_files)):
                       subplot_letter=subplot_letters[k],
                       plot_name=png_filename, best_fit=False)
 
-# ---------------------------------------------------------------
 
 lighthouse_clim = 'C:\\Users\\HourstonH\\Documents\\charles\\' \
                   'our_warming_ocean\\lighthouse_data\\' \
@@ -130,3 +330,48 @@ lighthouse_clim = 'C:\\Users\\HourstonH\\Documents\\charles\\' \
 plot_dir = os.path.dirname(lighthouse_clim)
 
 plot_climatology(lighthouse_clim, plot_dir)
+"""
+
+parent_dir = 'C:\\Users\\HourstonH\\Documents\\charles\\' \
+             'our_warming_ocean\\lighthouse_data\\' \
+ \
+    # Plot climatological monthly means at each station
+
+# Plot lighthouse temperature anomalies with linear trendline
+output_dir = parent_dir + 'monthly_anom_from_monthly_mean\\least_squares\\'
+anom_files = glob.glob(parent_dir + 'monthly_anom_from_monthly_mean\\*.csv',
+                       recursive=False)
+anom_files.sort()
+
+monthly_mean_files_dupe = glob.glob(
+    parent_dir + 'DATA_-_Active_Sites\\*\\*Average_Monthly_Sea_Surface_Temperatures*.csv')
+monthly_mean_files = [f for f in monthly_mean_files_dupe if 'french' not in f]
+monthly_mean_files.sort()
+
+station_names = [
+    os.path.basename(folder)
+    for folder in glob.glob(parent_dir + 'DATA_-_Active_Sites\\*')
+]
+
+for i in range(len(anom_files)):
+    plot_filled_sst_resid(
+        anom_files[i], sst_all_time_mean(monthly_mean_files[i]),
+        output_dir + f'{station_names[i]}_sst_resid_with_trend_and_mean.png')
+
+# ----------------------------------daily data-------------------------------
+
+daily_dir = 'C:\\Users\\HourstonH\\Documents\\charles\\our_warming_ocean\\' \
+            'lighthouse_data\\DATA_-_Active_Sites\\'
+
+daily_files_dupe = glob.glob(
+    daily_dir + '*\\*Daily_Sea_surface_Temperature*.csv')
+
+daily_files = [f for f in daily_files_dupe if 'french' not in f]
+
+output_dir = 'C:\\Users\\HourstonH\\Documents\\charles\\our_warming_ocean\\' \
+             'lighthouse_data\\daily_trend_plots\\'
+
+for f in daily_files:
+    stn = os.path.basename(f).split('_')[0] + '_' + os.path.basename(f).split('_')[1]
+    png_filename = output_dir + stn + '_trend_from_daily_sst.png'
+    plot_filled_daily_sst(f, png_filename)
